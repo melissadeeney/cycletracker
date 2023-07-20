269ca8abef2336301a1e9f4865c57073cab17119
melissadeeney <71728227+melissadeeney@users.noreply.github.com>
2023-07-20 22:03:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,12 @@ function calculateBMR() {
 
   // Convert values
   const weightInKg = (weight / 2.2).toFixed(2);
-  
+
+  // Calculate height in cm
+function calculateHeightInCm(heightFt, heightIn) {
+  const heightToCm = (heightFt * 12) + heightIn;
+  return heightToCm * 2.54;
+} 
 
   // Calculate BMR
   let bmr = 447.593 + (9.247 * weightInKg) + (3.098 * heightInCm);
